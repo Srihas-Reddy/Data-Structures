36dbf75b5e9383a89d4e68d0f2eae6e40834f077
Srihas-Reddy <158589375+Srihas-Reddy@users.noreply.github.com>
2025-09-27 18:09:47 +0000

    Printing a Linked List in reverse order.

---
+++ Linked List/ReverseTraversingLL.java
@@ -0,0 +1,18 @@
+class Node{
+    int data;
+    Node next;
+    Node() {}
+    Node(int data) { this.data = data; }
+    Node(int data, Node next) { this.data = data; this.next = next;}
+}
+
+public class ReverseTraversingLL {
+    public static void main(String[] args){
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        first.next = second;
+        second.next = third;
+        third.next = null;
+    }
+}
